06d5a060a47555fd899c7a12f003883e7d311892
Dmytro Orgish <isnl.dd@gmail.com>
2021-02-02 13:33:22 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -14,24 +14,26 @@ class Timer {
   }
 
   counter () {
-    const time = this.targetDate - Date.now();
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    setInterval(() => {
+      const time = this.targetDate - Date.now();
+      const secs = Math.floor((time % (1000 * 60)) / 1000);
+      const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+      const hours = Math.floor(
+        (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      );
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
-    this.refs.days.textContent = days;
-    this.refs.hours.textContent = hours;
-    this.refs.mins.textContent = mins;
-    this.refs.secs.textContent = secs;
+      this.refs.days.textContent = days;
+      this.refs.hours.textContent = hours;
+      this.refs.mins.textContent = mins;
+      this.refs.secs.textContent = secs;
+    }, 1000);
   }
 }
 
-const targetDate = new Timer({
+const timer = new Timer({
   selector: '#timer-1',
   targetDate: new Date('Sep 17, 2021'),
 });
 
-setInterval(() => {
-  targetDate.counter();
-}, 1000);
+timer.counter();
