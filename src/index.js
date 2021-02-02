3553cef49b52c2ef2f4c40e3dec4efb9ed05b013
Dmytro Orgish <isnl.dd@gmail.com>
2021-02-02 05:56:56 +0200

    finish

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import './styles.css';
+
+class Timer {
+  refs = {
+    days: document.querySelector('[data-value=days]'),
+    hours: document.querySelector('[data-value=hours]'),
+    mins: document.querySelector('[data-value=mins]'),
+    secs: document.querySelector('[data-value=secs]'),
+  };
+
+  constructor ({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  counter () {
+    const time = this.targetDate - Date.now();
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    this.refs.days.textContent = days;
+    this.refs.hours.textContent = hours;
+    this.refs.mins.textContent = mins;
+    this.refs.secs.textContent = secs;
+  }
+}
+
+const targetDate = 'Sep 17, 2024';
+
+setInterval(() => {
+  new Timer({
+    selector: '#timer-1',
+    targetDate: new Date(targetDate),
+  }).counter();
+}, 1000);
